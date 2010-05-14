ffcb08d64886de92ae7c575327eff5f74c5b8388
Albert Gasset <albert.gasset@gmail.com>
2010-05-14 13:25:05 +0200

    db: create index for scale fields

---
+++ db/upgrade.php
@@ -0,0 +1,22 @@
+<?php
+
+function xmldb_local_upgrade($oldversion) {
+    global $CFG;
+
+    $result = true;
+
+    $index = array('assignment' => 'grade',
+                   'forum' => 'scale',
+                   'glossary' => 'scale',
+                   'journal' => 'assessed');
+    foreach ($index as $table => $field) {
+        $table = new XMLDBTable($table);
+        $index = new XMLDBIndex($field);
+        $index->setAttributes(XMLDB_INDEX_NOTUNIQUE, array($field));
+        if ($result && !index_exists($table, $index)) {
+            $result = add_index($table, $index, false);
+        }
+    }
+
+    return $result;
+}
---
+++ version.php
@@ -0,0 +1,3 @@
+<?php
+
+$local_version = 2010051400;

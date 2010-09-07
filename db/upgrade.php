5270ad9d2e6d8f1779b4af0210178c7489819634
Albert Gasset <albert.gasset@gmail.com>
2010-09-07 12:18:04 +0200

    db: create local tables before creating indices

--- db/upgrade.php
+++ db/upgrade.php
@@ -9,21 +9,6 @@ function xmldb_local_upgrade($oldversion) {
     $xmldb_file->loadXMLStructure();
     $structure = $xmldb_file->getStructure();
 
-    $index = array('assignment' => 'grade',
-                   'forum' => 'scale',
-                   'glossary' => 'scale',
-                   'journal' => 'assessed',
-                   'questionnaire_quest_choice' => 'question_id',
-                   'local_batch_job' => 'timecreated');
-    foreach ($index as $table => $field) {
-        $table = new XMLDBTable($table);
-        $index = new XMLDBIndex($field);
-        $index->setAttributes(XMLDB_INDEX_NOTUNIQUE, array($field));
-        if ($result && !index_exists($table, $index)) {
-            $result = add_index($table, $index, false);
-        }
-    }
-
     $table = new XMLDBTable('batch_job');
     if ($result and table_exists($table)) {
         $result = rename_table($table, 'local_batch_job', false);
@@ -37,6 +22,21 @@ function xmldb_local_upgrade($oldversion) {
     foreach ($structure->getTables() as $table) {
         if ($result and !table_exists($table)) {
             $result = create_table($table, false);
+        }
+    }
+
+    $index = array('assignment' => 'grade',
+                   'forum' => 'scale',
+                   'glossary' => 'scale',
+                   'journal' => 'assessed',
+                   'questionnaire_quest_choice' => 'question_id',
+                   'local_batch_job' => 'timecreated');
+    foreach ($index as $table => $field) {
+        $table = new XMLDBTable($table);
+        $index = new XMLDBIndex($field);
+        $index->setAttributes(XMLDB_INDEX_NOTUNIQUE, array($field));
+        if ($result && !index_exists($table, $index)) {
+            $result = add_index($table, $index, false);
         }
     }
 

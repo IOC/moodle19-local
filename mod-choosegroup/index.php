5486fca94d9002001bd51efe05dacb58c321a604
Albert Gasset <albert.gasset@gmail.com>
2010-11-03 16:41:57 +0100

    choosegroup: copyright and readme

--- mod-choosegroup/index.php
+++ mod-choosegroup/index.php
@@ -1,4 +1,21 @@
 <?php
+/* Copyright © 2010 Institut Obert de Catalunya
+
+   This file is part of Choose Group.
+
+   Choose Group is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Choose Group is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 require_once(dirname(__FILE__).'/lib.php');

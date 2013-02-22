0ea5692938687845d7d257fca6e78a76a89e3306
Albert Gasset <albert.gasset@gmail.com>
2013-02-22 09:08:01 +0100

    batch: exporta materials si configurat

--- batch/types/export_course.php
+++ batch/types/export_course.php
@@ -3,21 +3,26 @@
 class batch_type_export_course extends batch_type_base {
 
     function execute($params) {
+        global $CFG;
+
         if (!$course = get_record('course', 'shortname',
                                   addslashes($params->shortname))) {
             throw new Exception('nonexistent');
         }
 
         $materials = false;
-        $records = get_records('local_materials', 'course', $course->id);
-        if ($records and count($records) == 1) {
-            $record = reset($records);
-            $select = "path = '" . addslashes($record->path) . "' AND course != $course->id";
-            if ($id = get_field('course', 'id', 'shortname', $course->shortname . '~')) {
-                $select .= " AND course != $id";
-            }
-            if (!record_exists_select('local_materials', $select)) {
-                $materials = $record->path;
+        if ($params->materials and !empty($CFG->local_materials_dir)) {
+            $records = get_records('local_materials', 'course', $course->id);
+            if ($records and count($records) == 1) {
+                $record = reset($records);
+                $select = "path = '" . addslashes($record->path) . "' AND course != $course->id";
+                if ($id = get_field('course', 'id', 'shortname', $course->shortname . '~')) {
+                    $select .= " AND course != $id";
+                }
+                if (!record_exists_select('local_materials', $select)
+                    and is_dir($CFG->local_materials_dir.'/'.$record->path)) {
+                    $materials = $record->path;
+                }
             }
         }
 
